ad12ece2840479ca84352aa5c0b249bd4acd1954
Sahil Chouksey <59500196+sahilchouksey@users.noreply.github.com>
2020-04-16 22:45:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // update the UL (user interface)
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     // check IF player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
 
